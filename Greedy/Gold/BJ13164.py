3efd04b24a09d8cbeebfab6fb50cc74af6682b2b
Jeongjewww <gwmelody99@gmail.com>
2022-07-23 02:11:17 +0900

    0723 commit

---
+++ Greedy/Gold/BJ13164.py
@@ -0,0 +1,11 @@
+# 행복 유치원
+
+n, k = map(int, input().split())
+stu = list(map(int, input().split()))
+
+diff = []
+for i in range(n-1):
+    diff.append(stu[i+1]-stu[i])
+diff.sort(reverse=True)
+
+print(sum(diff[k-1:]))
